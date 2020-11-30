388d19aa4a5896cd370edb1612addc4a32aadd43
boyke <boyke1212@gmail.com>
2020-12-01 04:19:02 +0700

    final

--- app/src/main/java/com/toelve/coba/Home.java
+++ app/src/main/java/com/toelve/coba/Home.java
@@ -9,10 +9,14 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Home extends AppCompatActivity {
     private String response;
     private SharedPreferences boyprefs;
-private EditText etresop;
+private EditText etresop,etNama,etToken;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +24,23 @@ private EditText etresop;
         setContentView(R.layout.activity_home);
         response=boyprefs.getString("response","");
         etresop=findViewById(R.id.etresop);
+        etNama=findViewById(R.id.etNama);
+        etToken=findViewById(R.id.etToken);
         etresop.setText(response);
+        JSONArray as= null;
+        try {
+            JSONObject jsonObj = new JSONObject(response);
+            String ass=jsonObj.getString("data");
+            JSONObject jsonObj2 = new JSONObject(ass);
+            etNama.setText(jsonObj2.getString("fullName"));
+            etToken.setText(jsonObj2.getString("jwtToken"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+
+
     }
 
     @Override
